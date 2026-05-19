6e11ca15c06045bd0cf332f8b029caea5db6d4b5
tahin <mdtahinhassanjihad@gmail.com>
2026-05-19 23:29:09 +0600

    title method

---
+++ 3-layout-navigation/src/app/(root)/blog/contact/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata ={
+    title:{
+        absolute:"Blog Contact Page"
+    }
+}
+
+function page() {
+  return (
+    <div>
+      contact  page
+    </div>
+  )
+}
+
+export default page
