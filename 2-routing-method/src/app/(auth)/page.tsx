efaff65ff06d1630b39846110783924df8553bf8
tahin <mdtahinhassanjihad@gmail.com>
2026-05-19 16:26:32 +0600

    Private , () method

---
+++ 2-routing-method/src/app/(auth)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      {/* (auth) -> folder a () use kolar janno url a auth deta hova na */}
+   {/* url search : localhost:3000/login  */}
+   {/* url search : localhost:3000/register  */}
+    </div>
+  )
+}
+
+export default page
---
+++ 2-routing-method/src/app/6private_folder/_book/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function page() {
+  return (
+
+    //! kono folder ar first _ user kola private folder hoy
+
+    <div>
+      <h1>private folder method book</h1>
+    </div>
+  )
+}
+
+export default page
