715b7615f8b3531e8e7008680b0e4f717e3c45e4
tahin <mdtahinhassanjihad@gmail.com>
2026-06-20 11:57:54 +0600

    multiple query search method

--- 6-api-route-handlers/app/1RequestMethod/route.ts
+++ 6-api-route-handlers/app/1RequestMethod/route.ts
@@ -1,34 +1,68 @@
 import { user } from "../data/user";
+import { NextRequest, NextResponse } from "next/server";
+
+
+//?  user type method 
+interface Address{
+    city?:string
+} ;
+interface User{
+    id:number ,
+    name : string ,
+    email : string ,
+    address?: Address
+}
+
+//! multiple query search method
+export async function GET(request:NextRequest){
+    const searchParams = request.nextUrl.searchParams ;
+    let filterUsers : User[] = [...user] ;
+
+    const filterMapping :{
+      [key:string] :(users:User[], value:string) => User[]
+    } = {
+        query:(users,value) => users.filter(user => user.name.toLowerCase().includes(value.toLowerCase())),
+        city:(users,value) => users.filter(user => user?.address?.city?.toLowerCase().includes(value.toLowerCase())) 
+        //* add more filter map value
+    }
+
+    for (const [key,value] of searchParams.entries()){
+        const lowerKey = key.toLowerCase() ;
+        if(filterMapping[lowerKey]){
+            filterUsers = filterMapping[lowerKey](filterUsers,value)
+        }
+    }
+
+    return NextResponse.json(filterUsers)
+}
+
 
 //! get method
-// export async function GET(){
-//     return Response.json(user)
-// }
+// ? necar get function janno ai function comment korce
+export async function GET(){
+    return Response.json(user)
+}
 
 //! post method 
 export async function POST(request:Request){
    try {
      const userData = await request.json() ;
-
     const newUser = {
-        id: user.length + 1 ,
+        id: user.length + 1 ,    "genre": "Dark Academia",
         ...userData 
     }
-
     user.push(newUser) ;
-
     return new Response (JSON.stringify(newUser) , {
         headers:{
             "Content-Type" : "application/json"
         },
-        status:201
+        status:201 
     } )
    } catch (error) {
     console.log(error);
    } 
 }
 
-import { NextRequest } from "next/server";
 
 
 //!  query search method
@@ -37,9 +71,9 @@ export async function GET(request:NextRequest){
   const searchParams = request.nextUrl.searchParams ;
 
   //* get -> ja name ar oper base kola search kola hova
-  const query = searchParams.get('query')?.trim() ;
+  const query = searchParams.get('query') ;
 
-  const filterUser = query ? user.filter(data => data.title?.toLowerCase().includes(query.toLowerCase()) ): user ;
+  const filterUser = query ? user.filter(data => data.name?.toLowerCase().includes(query.toLowerCase()) ): user ;
 
-  return Response.json(filterUser)
+  return Response.json(filterUser) 
 }
--- 6-api-route-handlers/app/data/user.ts
+++ 6-api-route-handlers/app/data/user.ts
@@ -1,47 +1,141 @@
  export const user = [
 
     {
-      "id":1,
-      "title": "The Hitchhiker's Guide to the Galaxy",
-      "author": "Douglas Adams",
-      "description": "Arthur Dent's unlucky day gets worse when his house is demolished, the planet Earth is destroyed, and he is whisked through space.",
-      "genre": "Science Fiction",
-      "publicationYear": 1979,
-      "isbn": "978-0345391803",
-      "price": 8.99,
-      "isAvailable": true
-    },
-    {
-      "id":2,
-      "title": "Pride and Prejudice",
-      "author": "Jane Austen",
-      "description": "A novel of manners, following the emotional development of Elizabeth Bennet, who learns the repercussions of hasty judgments.",
-      "genre": "Romance",
-      "publicationYear": 1813,
-      "isbn": "978-0141439518",
-      "price": 7.50,
-      "isAvailable": true
-    },
-    {
-      "id":3,
-      "title": "1984",
-      "author": "George Orwell",
-      "description": "A dystopian social science fiction novel and cautionary tale about the dangers of totalitarianism.",
-      "genre": "Dystopian",
-      "publicationYear": 1949,
-      "isbn": "978-0451524935",
-      "price": 9.50,
-      "isAvailable": false
-    },
-    {
-      "id":4,
-      "title": "The Secret History",
-      "author": "Donna Tartt",
-      "description": "A group of classics students at a New England college fall into a world of morally dubious choices.",
-      "genre": "Dark Academia",
-      "publicationYear": 1992,
-      "isbn": "978-0679774916",
-      "price": 12.00,
-      "isAvailable": true
-    }
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      },
+      {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+          "street": "Victor Plains",
+          "suite": "Suite 879",
+          "city": "Wisokyburgh",
+          "zipcode": "90566-7771",
+          "geo": {
+            "lat": "-43.9509",
+            "lng": "-34.4618"
+          }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+          "name": "Deckow-Crist",
+          "catchPhrase": "Proactive didactic contingency",
+          "bs": "synergize scalable supply-chains"
+        }
+      },
+      {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+          "street": "Douglas Extension",
+          "suite": "Suite 847",
+          "city": "McKenziehaven",
+          "zipcode": "59590-4157",
+          "geo": {
+            "lat": "-68.6102",
+            "lng": "-47.0653"
+          }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+          "name": "Romaguera-Jacobson",
+          "catchPhrase": "Face to face bifurcated interface",
+          "bs": "e-enable strategic applications"
+        }
+      },
+      {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+          "street": "Hoeger Mall",
+          "suite": "Apt. 692",
+          "city": "South Elvis",
+          "zipcode": "53919-4257",
+          "geo": {
+            "lat": "29.4572",
+            "lng": "-164.2990"
+          }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+          "name": "Robel-Corkery",
+          "catchPhrase": "Multi-tiered zero tolerance productivity",
+          "bs": "transition cutting-edge web services"
+        }
+      },
+      {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+          "street": "Skiles Walks",
+          "suite": "Suite 351",
+          "city": "Roscoeview",
+          "zipcode": "33263",
+          "geo": {
+            "lat": "-31.8129",
+            "lng": "62.5342"
+          }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+          "name": "Keebler LLC",
+          "catchPhrase": "User-centric fault-tolerant solution",
+          "bs": "revolutionize end-to-end systems"
+        }
+      },
+      {
+        "id": 6,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "ABC",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      }
  ]
